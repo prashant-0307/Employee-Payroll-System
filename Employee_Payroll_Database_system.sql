ae4f86bd870e17c6d00e69c270c0e6501cf902b3
gardas123 <prashaantgardas64@gmail.com>
2025-02-12 14:33:05 +0530

    created all tables

--- Employee_Payroll_Database_system.sql
+++ Employee_Payroll_Database_system.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE EmployeePayrollSystem;
 use EmployeePayrollSystem;
 
-CREATE TABLE Employee {
+CREATE TABLE Employee(
 	Employee_id int primary key auto_increment,
     FirstName varchar(50),
     LastName varchar(50),
@@ -9,9 +9,29 @@ CREATE TABLE Employee {
     Email varchar(15) unique,
     Phone varchar(15),
     HireDate date
-};
+);
 
-create table Salary{
-	Salary_id int primary key auto_increment
-}
+create table Salary(
+	Salary_id int primary key auto_increment,
+    Employee_id int,
+    BasicPay decimal(10,2),
+    Allowance decimal(10,2),
+    Deductions decimal(10,2),
+    foreign key (Employee_id) references Employee(Employee_id) on delete cascade
+);
 
+create table Tax(
+	Tax_id int primary key auto_increment,
+    Employee_id int,
+    TaxAmount decimal(10,2),
+    ProvindentFund decimal(10,2),
+    foreign key (Employee_id) references Employee(Employee_id) on delete cascade
+);
+
+create table Payroll(
+	Payroll_id int primary key auto_increment,
+    Employee_id int,
+    SalaryMonth date,
+    NetSalary decimal(10,2),
+    foreign key (Employee_id) references Employee(Employee_id) on delete cascade
+);
